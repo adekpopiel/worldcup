adc8c40fd90b725d12f860050a32ecf8d361ae9c
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 16:19:04 +0200

    Adding mockito to the module

--- usecase/build.gradle.kts
+++ usecase/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation(project(":domain"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:2.1.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.6.0")
 }
 
 tasks.test {
