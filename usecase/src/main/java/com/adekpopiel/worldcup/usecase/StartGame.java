e6b07217388147133e0b9d1831340db3a4b285cf
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 18:54:04 +0200

    Logic for starting the game and persisting it

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/StartGame.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/StartGame.java
@@ -1,8 +1,12 @@
 package com.adekpopiel.worldcup.usecase;
 
 import com.adekpopiel.worldcup.domain.entity.Game;
+import com.adekpopiel.worldcup.usecase.exception.GameAlreadyExistException;
 import com.adekpopiel.worldcup.usecase.port.DateFormatter;
 import com.adekpopiel.worldcup.usecase.port.GameRepository;
+import com.adekpopiel.worldcup.usecase.validation.GameValidator;
+
+import java.util.Date;
 
 public class StartGame {
 
@@ -10,19 +14,23 @@ public class StartGame {
     private DateFormatter dateFormatter;
 
     public Game startGame(final Game game) {
-        return null;
-    }
-
-    public GameRepository getGameRepository() {
-        return gameRepository;
+        GameValidator.validate(game);
+        if (gameRepository.findGameById(game.getId()) != null) {
+            throw new GameAlreadyExistException("Game already exists!");
+        }
+        Game gameForCreate = Game.builder()
+                .id(game.getId())
+                .homeTeam(game.getHomeTeam())
+                .visitors(game.getVisitors())
+                .homeTeamScore(0)
+                .visitorsScore(0)
+                .startTime(dateFormatter.formatDate(new Date()))
+                .build();
+        return gameRepository.create(gameForCreate);
     }
 
     public void setGameRepository(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
-    }
-
-    public DateFormatter getDateFormatter() {
-        return dateFormatter;
     }
 
     public void setDateFormatter(DateFormatter dateFormatter) {
