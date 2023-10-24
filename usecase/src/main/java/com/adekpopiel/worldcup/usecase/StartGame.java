2a1cccfc7d07a4d7bc5bc5c4dc9092158e2a4adb
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 16:21:25 +0200

    Adding dependencies to the start game use case

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/StartGame.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/StartGame.java
@@ -1,4 +1,32 @@
 package com.adekpopiel.worldcup.usecase;
 
+import com.adekpopiel.worldcup.domain.entity.Game;
+import com.adekpopiel.worldcup.usecase.port.DateFormatter;
+import com.adekpopiel.worldcup.usecase.port.GameRepository;
+
 public class StartGame {
+
+    private GameRepository gameRepository;
+    private DateFormatter dateFormatter;
+
+    public Game startGame(final Game game) {
+        return null;
+    }
+
+    public GameRepository getGameRepository() {
+        return gameRepository;
+    }
+
+    public void setGameRepository(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public DateFormatter getDateFormatter() {
+        return dateFormatter;
+    }
+
+    public void setDateFormatter(DateFormatter dateFormatter) {
+        this.dateFormatter = dateFormatter;
+    }
+
 }
