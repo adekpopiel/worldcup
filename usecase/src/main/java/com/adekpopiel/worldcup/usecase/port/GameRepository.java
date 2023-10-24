c706918da7b8d85982199429659937b29265de8c
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 16:20:45 +0200

    Add searching for a game by id to the repository

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/port/GameRepository.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/port/GameRepository.java
@@ -3,6 +3,7 @@ package com.adekpopiel.worldcup.usecase.port;
 import com.adekpopiel.worldcup.domain.entity.Game;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface GameRepository {
     Game create(Game game);
@@ -12,4 +13,6 @@ public interface GameRepository {
     void update(Game game);
 
     void remove(Game game);
+
+    Game findGameById(UUID id);
 }
