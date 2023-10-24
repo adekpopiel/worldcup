1c5ca95ea18c8fe53a76e76abe85afef8a10f63f
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 18:48:25 +0200

    More tests for starting game use case

--- usecase/src/test/java/com/adekpopiel/worldcup/usecase/StartGameTest.java
+++ usecase/src/test/java/com/adekpopiel/worldcup/usecase/StartGameTest.java
@@ -2,25 +2,32 @@ package com.adekpopiel.worldcup.usecase;
 
 import com.adekpopiel.worldcup.domain.entity.Game;
 import com.adekpopiel.worldcup.usecase.exception.GameAlreadyExistException;
+import com.adekpopiel.worldcup.usecase.exception.GameValidationException;
 import com.adekpopiel.worldcup.usecase.port.DateFormatter;
 import com.adekpopiel.worldcup.usecase.port.GameRepository;
+import com.adekpopiel.worldcup.usecase.validation.GameValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StartGameTest {
 
-    public static final String GAME_NOT_FOUND_MESSAGE = "Game was not found!";
+    public static final String GAME_ALREADY_EXISTS = "Game already exists!";
+    public static final String GAME_CANNOT_BE_NULL = "Game cannot be null";
+    public static final String HOME_TEAM_NAME_HAS_TO_BE_SET = "Home team name has to be set";
+    public static final String VISITORS_TEAM_NAME_HAS_TO_BE_SET = "Visitors team name has to be set";
     @InjectMocks
     private StartGame startGameUseCase;
 
@@ -31,16 +38,48 @@ class StartGameTest {
     private DateFormatter dateFormatter;
 
     @Test
-    public void testNewGameIsStarted() {
+    public void testNewGameIsPersistedInRepository() {
         //given
+        UUID gameId = UUID.randomUUID();
         Game newGame = Game.builder()
+                .id(gameId)
                 .homeTeam("X")
                 .visitors("Y")
                 .build();
         //when
         startGameUseCase.startGame(newGame);
         //then
-        verify(gameRepository).create(newGame);
+        verify(gameRepository).create(any(Game.class));
+    }
+
+    @Test
+    public void testGameExistenceIsChecked() {
+        //given
+        UUID gameId = UUID.randomUUID();
+        Game newGame = Game.builder()
+                .id(gameId)
+                .homeTeam("X")
+                .visitors("Y")
+                .build();
+        //when
+        startGameUseCase.startGame(newGame);
+        //then
+        verify(gameRepository).findGameById(gameId);
+    }
+
+    @Test
+    public void testStartDateIsFormatted() {
+        //given
+        UUID gameId = UUID.randomUUID();
+        Game newGame = Game.builder()
+                .id(gameId)
+                .homeTeam("X")
+                .visitors("Y")
+                .build();
+        //when
+        startGameUseCase.startGame(newGame);
+        //then
+        verify(dateFormatter).formatDate(any(Date.class));
     }
 
     @Test
@@ -56,7 +95,53 @@ class StartGameTest {
         when(gameRepository.findGameById(gameId)).thenReturn(newGame);
         GameAlreadyExistException exception = assertThrows(GameAlreadyExistException.class, () -> startGameUseCase.startGame(newGame));
         //then
-        assertTrue(exception.getMessage().contains(GAME_NOT_FOUND_MESSAGE));
+        assertTrue(exception.getMessage().contains(GAME_ALREADY_EXISTS));
+    }
+
+    @Test
+    public void testGameHomeTeamIsNotNull() {
+        Game game = Game.builder()
+                .visitors("Y")
+                .build();
+        GameValidationException exception = assertThrows(
+                GameValidationException.class, () -> startGameUseCase.startGame(game));
+        assertTrue(exception.getMessage().contains(HOME_TEAM_NAME_HAS_TO_BE_SET));
+
+    }
+
+    @Test
+    public void testGameHomeTeamIsNotEmpty() {
+        Game game = Game.builder()
+                .homeTeam("")
+                .visitors("Y")
+                .build();
+        GameValidationException exception = assertThrows(
+                GameValidationException.class, () -> startGameUseCase.startGame(game));
+        assertTrue(exception.getMessage().contains(HOME_TEAM_NAME_HAS_TO_BE_SET));
+
+    }
+
+    @Test
+    public void testGameVisitorsIsNotNull() {
+        Game game = Game.builder()
+                .homeTeam("Y")
+                .build();
+        GameValidationException exception = assertThrows(
+                GameValidationException.class, () -> startGameUseCase.startGame(game));
+        assertTrue(exception.getMessage().contains(VISITORS_TEAM_NAME_HAS_TO_BE_SET));
+
+    }
+
+    @Test
+    public void testGameVisitorsIsNotEmpty() {
+        Game game = Game.builder()
+                .homeTeam("X")
+                .visitors("")
+                .build();
+        GameValidationException exception = assertThrows(
+                GameValidationException.class, () -> startGameUseCase.startGame(game));
+        assertTrue(exception.getMessage().contains(VISITORS_TEAM_NAME_HAS_TO_BE_SET));
+
     }
 
 
