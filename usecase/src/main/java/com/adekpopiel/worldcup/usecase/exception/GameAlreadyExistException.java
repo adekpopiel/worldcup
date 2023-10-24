70e88f44f9f1d5fed5ae2b7d9ec8b934922fb862
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 18:48:01 +0200

    Adding proper constructor for game existence exception

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/exception/GameAlreadyExistException.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/exception/GameAlreadyExistException.java
@@ -1,5 +1,7 @@
 package com.adekpopiel.worldcup.usecase.exception;
 
 public class GameAlreadyExistException extends RuntimeException {
-
+    public GameAlreadyExistException(String message) {
+        super(message);
+    }
 }
