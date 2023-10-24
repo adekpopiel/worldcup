8eca8b727e998fea649023ca834903681e2977d7
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-24 16:44:25 +0200

    Adding game validation logic

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/validation/GameValidator.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/validation/GameValidator.java
@@ -1,6 +1,7 @@
 package com.adekpopiel.worldcup.usecase.validation;
 
 import com.adekpopiel.worldcup.domain.entity.Game;
+import com.adekpopiel.worldcup.usecase.exception.GameValidationException;
 
 public class GameValidator {
 
@@ -8,7 +9,11 @@ public class GameValidator {
 
     }
 
-    public static void validateGameForCreation(final Game game) {
-
+    public static void validate(final Game game) {
+        if (game == null) throw new GameValidationException("Game cannot be null");
+        if (game.getHomeTeam() == null) throw new GameValidationException("Home team name has to be set");
+        if (game.getHomeTeam().isEmpty()) throw new GameValidationException("Home team name has to be set");
+        if (game.getVisitors() == null) throw new GameValidationException("Visitors team name has to be set");
+        if (game.getVisitors().isEmpty()) throw new GameValidationException("Visitors team name has to be set");
     }
 }
