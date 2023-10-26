5f268d598fb162fe61402bda148ae3f75d858968
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-26 16:20:46 +0200

    Dependencies for FinishGame use case added

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/FinishGame.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/FinishGame.java
@@ -1,4 +1,20 @@
 package com.adekpopiel.worldcup.usecase;
 
+import com.adekpopiel.worldcup.domain.entity.Game;
+import com.adekpopiel.worldcup.usecase.port.GameRepository;
+
+import java.util.UUID;
+
 public class FinishGame {
+
+    private GameRepository gameRepository;
+
+
+    public void finishGame(final UUID gameIdForFinish) {
+
+    }
+
+    public void setGameRepository(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
 }
