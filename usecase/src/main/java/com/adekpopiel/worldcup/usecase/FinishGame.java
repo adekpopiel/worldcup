da86216fe9bae99952fcba9ccdf75047030869ee
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-26 16:32:30 +0200

    Logic for FinishGame use case

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/FinishGame.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/FinishGame.java
@@ -1,6 +1,7 @@
 package com.adekpopiel.worldcup.usecase;
 
 import com.adekpopiel.worldcup.domain.entity.Game;
+import com.adekpopiel.worldcup.usecase.exception.GameNotFoundException;
 import com.adekpopiel.worldcup.usecase.port.GameRepository;
 
 import java.util.UUID;
@@ -10,8 +11,15 @@ public class FinishGame {
     private GameRepository gameRepository;
 
 
-    public void finishGame(final UUID gameIdForFinish) {
-
+    public void finishGame(final UUID gameId) {
+        if (gameId == null) {
+            throw new IllegalArgumentException("Input parameter cannot be null: gameId");
+        }
+        Game gameForRemoval = gameRepository.findGameById(gameId);
+        if (gameForRemoval == null) {
+            throw new GameNotFoundException("Game for removal was not found!");
+        }
+        gameRepository.remove(gameForRemoval);
     }
 
     public void setGameRepository(GameRepository gameRepository) {
