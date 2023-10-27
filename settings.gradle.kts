ffae570b291daf960f930c977e5d57c127999e1b
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-27 16:49:13 +0200

    Adding new modules to the gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
 rootProject.name = "worldcup"
 include("domain")
 include("usecase")
+include("adapter:controller")
+findProject(":adapter:controller")?.name = "controller"
+include("adapter:repository:in-memory")
+findProject(":adapter:repository:in-memory")?.name = "in-memory"
+include("adapter:dateformatter:iso8601")
+findProject(":adapter:dateformatter:iso8601")?.name = "iso8601"
