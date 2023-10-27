e34e2985be48952185e09787d063132b004bba46
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-27 08:48:31 +0200

    Adding printStream dependendy to the PrintGame use case

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/PrintGame.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/PrintGame.java
@@ -2,11 +2,23 @@ package com.adekpopiel.worldcup.usecase;
 
 import com.adekpopiel.worldcup.usecase.port.GameRepository;
 
+import java.io.PrintStream;
+
 public class PrintGame {
 
     private GameRepository gameRepository;
 
+    private PrintStream printStream;
+
     public void printScoreboard() {
 
+    }
+
+    public void setGameRepository(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public void setPrintStream(PrintStream printStream) {
+        this.printStream = printStream;
     }
 }
