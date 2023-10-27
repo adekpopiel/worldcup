5b18950d0e665a52c96e0da560b72d11fcfcb109
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-27 17:03:24 +0200

    Adding empty GameController body

--- adapter/controller/src/main/java/com/adekpopiel/worldcup/controller/GameController.java
+++ adapter/controller/src/main/java/com/adekpopiel/worldcup/controller/GameController.java
@@ -1,4 +1,28 @@
 package com.adekpopiel.worldcup.controller;
 
+import com.adekpopiel.worldcup.usecase.FinishGame;
+import com.adekpopiel.worldcup.usecase.PrintGame;
+import com.adekpopiel.worldcup.usecase.StartGame;
+import com.adekpopiel.worldcup.usecase.UpdateGame;
+
 public class GameController {
+
+    private final StartGame startGameUseCase;
+    private final UpdateGame updateGameUseCase;
+
+    private final FinishGame finishGameUseCase;
+
+    private final PrintGame printGameUseCase;
+
+    public GameController(final StartGame startGameUseCase,
+                          final UpdateGame updateGameUseCase,
+                          final FinishGame finishGameUseCase,
+                          final PrintGame printGameUseCase) {
+        this.startGameUseCase = startGameUseCase;
+        this.updateGameUseCase = updateGameUseCase;
+        this.finishGameUseCase = finishGameUseCase;
+        this.printGameUseCase = printGameUseCase;
+    }
+
+
 }
