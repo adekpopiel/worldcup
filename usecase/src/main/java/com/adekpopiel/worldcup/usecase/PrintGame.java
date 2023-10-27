81b09e1af5d483e3bbc7e8ea66e3d878e3422642
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-27 16:10:39 +0200

    Logic for PrintGame use case

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/PrintGame.java
+++ usecase/src/main/java/com/adekpopiel/worldcup/usecase/PrintGame.java
@@ -1,17 +1,60 @@
 package com.adekpopiel.worldcup.usecase;
 
+import com.adekpopiel.worldcup.domain.entity.Game;
+import com.adekpopiel.worldcup.usecase.port.DateFormatter;
 import com.adekpopiel.worldcup.usecase.port.GameRepository;
 
 import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class PrintGame {
 
-    private GameRepository gameRepository;
+    private static final String GAME_TEMPLATE = "%s %d - %s %d";
 
+    private GameRepository gameRepository;
     private PrintStream printStream;
+    private DateFormatter dateFormatter;
+
 
     public void printScoreboard() {
+        List<Game> listOfAllGames = gameRepository.findAllGames();
+        listOfAllGames.stream()
+                .sorted(byTotalScoreAndStartTime())
+                .forEach(printGame());
+    }
 
+    private Consumer<Game> printGame() {
+        Supplier<Consumer<Game>> consumerSupplier = () -> game -> {
+            printStream.printf((GAME_TEMPLATE) + "\n",
+                    game.getHomeTeam(),
+                    game.getHomeTeamScore(),
+                    game.getVisitors(),
+                    game.getVisitorsScore());
+        };
+        return consumerSupplier.get();
+    }
+
+    private Comparator<Game> byTotalScoreAndStartTime() {
+        Supplier<Comparator<Game>> comparatorSupplier = () -> (game1, game2) -> {
+            int totalScoreComparison = Integer.compare(game2.getTotalScore(), game1.getTotalScore());
+            if (totalScoreComparison == 0) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormatter.getFormatterString());
+                LocalDateTime date1 = LocalDateTime.parse(
+                        game1.getStartTime(),
+                        formatter);
+                LocalDateTime date2 = LocalDateTime.parse(
+                        game2.getStartTime(),
+                        formatter);
+                return date2.compareTo(date1);
+            }
+            return totalScoreComparison;
+        };
+        return comparatorSupplier.get();
     }
 
     public void setGameRepository(GameRepository gameRepository) {
@@ -20,5 +63,9 @@ public class PrintGame {
 
     public void setPrintStream(PrintStream printStream) {
         this.printStream = printStream;
+    }
+
+    public void setDateFormatter(DateFormatter dateFormatter) {
+        this.dateFormatter = dateFormatter;
     }
 }
