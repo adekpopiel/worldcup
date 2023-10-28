004b930032ac6277ab9c9a624d0bb46617170cfc
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-28 08:54:30 +0200

    Logic for date formatter

--- adapter/dateformatter/iso8601/src/main/java/com/adekpopiel/worldcup/dateformatter/ISO8601DateFormatter.java
+++ adapter/dateformatter/iso8601/src/main/java/com/adekpopiel/worldcup/dateformatter/ISO8601DateFormatter.java
@@ -2,16 +2,24 @@ package com.adekpopiel.worldcup.dateformatter;
 
 import com.adekpopiel.worldcup.usecase.port.DateFormatter;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ISO8601DateFormatter implements DateFormatter {
+
+    private static final String ISO8601_DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+
     @Override
     public String formatDate(Date dateToFormat) {
-        return null;
+        if (dateToFormat == null) {
+            throw new IllegalArgumentException("Date cannot be null!");
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(ISO8601_DATE_TIME_PATTERN);
+        return sdf.format(dateToFormat);
     }
 
     @Override
     public String getFormatterString() {
-        return null;
+        return ISO8601_DATE_TIME_PATTERN;
     }
 }
