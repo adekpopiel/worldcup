180acbb18fdbc7bebd4b361c372cc6ef96b5d5b3
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-29 08:49:04 +0100

    Library documentation

--- usecase/src/main/java/com/adekpopiel/worldcup/usecase/exception/GameAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.adekpopiel.worldcup.usecase.exception;
-
-public class GameAlreadyExistException extends RuntimeException {
-    public GameAlreadyExistException(String message) {
-        super(message);
-    }
-}
