654392492f26a2757a7d1295af470c42ee5a3229
Adrian Popiel <adrian.popiel@ocado.com>
2023-10-29 07:09:09 +0100

    Logic for in memory repository

--- adapter/repository/in-memory/src/main/java/com/adekpopiel/worldcup/db/GameInMemoryRepository.java
+++ adapter/repository/in-memory/src/main/java/com/adekpopiel/worldcup/db/GameInMemoryRepository.java
@@ -3,32 +3,33 @@ package com.adekpopiel.worldcup.db;
 import com.adekpopiel.worldcup.domain.entity.Game;
 import com.adekpopiel.worldcup.usecase.port.GameRepository;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class GameInMemoryRepository implements GameRepository {
+
+    private Map<UUID, Game> repository;
     @Override
     public Game create(Game game) {
-        return null;
+        return repository.put(game.getId(), game);
     }
 
     @Override
     public List<Game> findAllGames() {
-        return null;
+        return new ArrayList<>(repository.values());
     }
 
     @Override
     public void update(Game game) {
-
+        repository.put(game.getId(), game);
     }
 
     @Override
     public void remove(Game game) {
-
+        repository.remove(game.getId());
     }
 
     @Override
     public Game findGameById(UUID id) {
-        return null;
+        return repository.get(id);
     }
 }
